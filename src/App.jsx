c6a6bc77bc75207311b084d598e4f7997071254d
StebanPi <pinedasteban13@gmail.com>
2025-08-02 17:14:10 -0500

    fixed link

--- src/App.jsx
+++ src/App.jsx
@@ -24,6 +24,7 @@ function App() {
             <Route path="/movie/:id" element={<MoviePage></MoviePage>} />
 
             <Route path="/search" element={<SearchPage></SearchPage>}></Route>
+            <Route path="/*" element={<IndexPage />}></Route>
           </Routes>
         </div>
       </ThemeProvider>
